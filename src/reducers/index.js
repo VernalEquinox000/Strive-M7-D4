e6e4a0007cfbd2dda339d1cb6d3e8b68b4d04df3
VernalEquinox <pieroapruzzese@gmail.com>
2021-02-06 23:39:23 +0100

    set up

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+export default function (state = {}, action) {
+    switch(action.type) {
+        case "SEARCH_JOBS":
+        return {
+            ...state
+        }
+        default: return state;
+    }
+
+    }
